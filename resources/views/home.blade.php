ee7a571fee4fc88f5a725c8d7877f02e241e4231
johnieje <johnieje@yahoo.com>
2016-11-09 16:02:50 -0500

    Adding custom css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,13 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
+            <form action="post" action="">
+                <div class="form-group">
+                    <label for="post-body">Write a new story</label>
+                    <textarea name="post-body" id="post-body" rows="5" class="form-control" placeholder="Share your story here..."></textarea>
                 </div>
-            </div>
+                <button type="submit" class="btn btn-primary">Share</button>
+            </form>
         </div>
     </div>
 </div>
