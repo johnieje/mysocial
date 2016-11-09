03bb03ed36b2b039d0269c441c234244ed22d596
johnieje <johnieje@yahoo.com>
2016-11-09 13:48:15 -0500

    adding a footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,10 +47,5 @@
     </nav>
 
     @yield('content')
-
-    <!-- JavaScripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
-</body>
-</html>
+@include('includes.footer')
+   
