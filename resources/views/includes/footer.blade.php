3ad93af077ae6664055445d3cf681722ca4d5f2b
johnieje <johnieje@yahoo.com>
2016-11-27 09:57:30 -0500

    Adding footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,17 @@
- <!-- JavaScripts -->
+<div class="container">
+  <div class="row">
+  <hr>
+    <div class="col-lg-12">
+      <div class="col-md-8">
+        <a href="#">Terms of Service</a> | <a href="#">Privacy</a>    
+      </div>
+      <div class="col-md-4">
+        <p class="muted pull-right">© 2013 Socialight. All rights reserved</p>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- JavaScripts -->
     <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
     <script src="https://code.jquery.com/jquery-migrate-3.0.0.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
